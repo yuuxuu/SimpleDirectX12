a24ae2f26284d4fe13651728d379ddcb937cc766
yuuxuu1308\kimura <kimura.yuu1120@gmail.com>
2025-06-20 00:00:09 +0900

    ・DX12PSOインターフェイスの追加 ・PSOフォルダーの構成を変更

---
+++ Sources/GraphicsAPI/DirectX12/DX12Pipline/PSO/DX12ComputePSO.cpp
@@ -0,0 +1,39 @@
+﻿/**
+ * @file ComputePipeline.cpp
+ * @brief
+ * @author Yu Kimura
+ * @date 2020/05/18
+ */
+
+#include "GraphicsAPI/DirectX12/DX12Pipline/PSO/DX12ComputePSO.h"
+
+#include "GraphicsAPI/Shader/Shader.h"
+
+namespace Graphics 
+{
+    // コンストラクタ
+    DX12ComputePSO::DX12ComputePSO()
+    {
+    }
+
+    // デストラクタ
+    DX12ComputePSO::~DX12ComputePSO()
+    {
+    }
+
+    void DX12ComputePSO::SetComputePipelineState(Shader::Shader* pShader, D3D12_COMPUTE_PIPELINE_STATE_DESC& computePipelineStateDesc)
+    {
+        auto m_pShaderMap = pShader->GetShaderMap();
+        for (const auto pShader : m_pShaderMap)
+        {
+            auto shaderByteCode = D3D12_SHADER_BYTECODE
+            {
+                pShader.second->GetBufferPointer(),
+                pShader.second->GetBufferSize()
+            };
+
+            if (pShader.first == "CS")
+                computePipelineStateDesc.CS = shaderByteCode;
+        }
+    }
+} // namespace Graphics
