3845852c9113135453375abd77b2c1d7103afd7e
yuuxuu1308\kimura <kimura.yuu1120@gmail.com>
2025-06-19 23:58:02 +0900

    ・PSO抽象ファクトリーを追加

---
+++ Sources/GraphicsAPI/DirectX12/DX12Pipline/PSOFactory/DX12PSOAbstractFactory.h
@@ -0,0 +1,23 @@
+﻿/**
+ * @file DX12PSOAbstractFactory.h
+ * @brief
+ * @author Yu Kimura
+ * @date 2025/06/19
+ */
+
+#pragma once
+
+#include "GraphicsAPI/DirectX12/DX12Pipline/PSOFactory/IDX12PSOAbstractFactory.h"
+
+namespace Graphics
+{
+	class DX12PSOAbstractFactory : IDX12PSOAbstractFactory
+	{
+	public:
+		DX12PSOAbstractFactory();
+		~DX12PSOAbstractFactory();
+
+		std::unique_ptr<IDX12PSO> CreatePSO(DX12Device* pDX12Device, Shader::Shader* pShader) override;
+	};
+} // namespace Graphics
+
