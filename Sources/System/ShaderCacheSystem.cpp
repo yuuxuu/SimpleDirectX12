748ad6b2a44c2fd901b2bd60f86327cf6b5473be
yuuxuu1308\kimura <kimura.yuu1120@gmail.com>
2025-09-16 00:17:01 +0900

    シェーダーキャッシュシステムを追加

---
+++ Sources/System/ShaderCacheSystem.cpp
@@ -0,0 +1,91 @@
+﻿/**
+ * @file ShaderCahceSystem.cpp
+ * @brief
+ * @author Yu Kimura
+ * @date 2025/07/07
+ */
+
+#include "ShaderCacheSystem.h"
+
+#include "GraphicsAPI/Shader/Shader.h"
+
+#include "GraphicsAPI/DirectX12/DX12Device.h"
+
+#include "GraphicsAPI/DirectX12/DX12Pipline/PSOFactory/DX12GraphicsPSOAbstractFactory.h"
+
+#include "Param/ShaderFileNameListParam.h"
+
+namespace Simple
+{
+namespace System
+{
+	ShaderCacheSystem::ShaderCacheSystem()
+	{ }
+
+	ShaderCacheSystem::~ShaderCacheSystem()
+	{ }
+
+	void ShaderCacheSystem::Initialize(Graphics::DX12Device* pDX12Device)
+	{
+        std::map<std::string, std::filesystem::path> filePathMap;
+
+        auto itr = std::filesystem::recursive_directory_iterator("Resources/Shader/HLSL");
+        for (const auto& file : itr)
+        {
+            auto extension = file.path().extension();
+            if (extension != ".hlsl")
+                continue;
+
+            filePathMap[file.path().filename().string()] = file.path();
+        }
+
+        Graphics::DX12GraphicsPSOAbstractFactory factory;
+        for (const auto& fileName : ShaderFileNameListParam::GetFileNameList())
+        {
+            auto itr = filePathMap.find(fileName);
+            if (itr == filePathMap.cend())
+            {
+                continue;
+            }
+
+            auto pShader = std::make_unique<Graphics::Shader::Shader>();
+            if (!pShader->Initialize(itr->second.string()))
+                return;
+
+            if (auto pPSO = factory.CreatePSO(pDX12Device, pShader.get()))
+            {
+                m_shaderMap[fileName] = ShaderPSOPair(std::move(pShader), std::move(pPSO));
+            }
+        }
+	}
+
+    void ShaderCacheSystem::SetCurrentShader(const std::string& shaderFileName)
+    {
+        if (m_pCurrentShader.first != shaderFileName)
+        {
+            auto itr = m_shaderMap.find(shaderFileName);
+            if (itr == m_shaderMap.cend())
+            {
+                m_pCurrentShader = {};
+
+                MessageBoxA(NULL, (shaderFileName + " が読み込まれていません。").c_str(), "MessageBox", MB_OK);
+                return;
+            }
+
+            m_pCurrentShader = { shaderFileName, &itr->second };
+        }
+    }
+
+    void ShaderCacheSystem::SetPipline(Graphics::DX12Command* pDX12Command)
+    {
+        if (!m_pCurrentShader.second)
+        {
+            return;
+        }
+
+        m_pCurrentShader.second->pPSO->SetRootSignature(pDX12Command);
+        m_pCurrentShader.second->pPSO->SetPipeline(pDX12Command);
+    }
+
+} // namespace System
+} // namespace Simple
---
+++ Sources/System/ShaderCacheSystem.h
@@ -0,0 +1,64 @@
+﻿/**
+ * @file ShaderCahceSystem.h
+ * @brief
+ * @author Yu Kimura
+ * @date 2025/07/07
+ */
+
+#pragma once
+
+#include "GraphicsAPI/Shader/IShader.h"
+#include "GraphicsAPI/DirectX12/DX12Pipline/PSO/IDX12PSO.h"
+
+namespace Graphics
+{
+	class DX12Device;
+	class DX12Command;
+}
+
+namespace Simple
+{
+namespace System
+{
+	struct ShaderPSOPair
+	{
+		std::unique_ptr<Graphics::Shader::IShader>	pShader;
+		std::unique_ptr<Graphics::IDX12PSO>			pPSO;
+
+		ShaderPSOPair()
+		{ }
+
+		ShaderPSOPair(std::unique_ptr<Graphics::Shader::IShader> pShader, std::unique_ptr<Graphics::IDX12PSO> pPSO):
+			pShader(std::move(pShader)),
+			pPSO(std::move(pPSO))
+		{}
+	};
+
+	using ShaderMap = std::map<std::string, ShaderPSOPair>;
+
+	class ShaderCacheSystem
+	{
+	private:
+		ShaderMap m_shaderMap;
+
+		std::pair<std::string, ShaderPSOPair*> m_pCurrentShader;
+
+	private:
+		ShaderCacheSystem();
+		~ShaderCacheSystem();
+
+	public:
+		static ShaderCacheSystem& GetSystem()
+		{
+			static ShaderCacheSystem system;
+			return system;
+		}
+
+		void Initialize(Graphics::DX12Device* pDX12Device);
+
+		void SetCurrentShader(const std::string& shaderFileName);
+
+		void SetPipline(Graphics::DX12Command* pDX12Command);
+	};
+} // namespace System
+} // namespace Simple
